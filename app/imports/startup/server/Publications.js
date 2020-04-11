b1d8146d8cb38f71e4a32c99e8a7e13a047f91f2
Devoider <Drakon3001@gmail.com>
2020-04-10 21:37:26 -1000

    Added all of the DBs with default data, they do not have restrictions

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,6 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
+import { FoodTrucksCollection } from '../../api/foodTrucks/FoodTruckCollection';
+import { HoursCollection } from '../../api/hours/HoursCollection';
+import { LocationCollection } from '../../api/location/LocationCollection';
+import { MenuItemCollection } from '../../api/menu/MenuItemCollection';
+import { RestaurantCollection } from '../../api/restaurant/RestaurantCollection';
+import { ReviewsCollection } from '../../api/reviews/Reviews';
 
 /** This subscription publishes only the documents associated with the logged in user */
 Meteor.publish('Stuff', function publish() {
@@ -15,6 +21,54 @@ Meteor.publish('Stuff', function publish() {
 Meteor.publish('StuffAdmin', function publish() {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Stuffs.find();
+  }
+  return this.ready();
+});
+
+Meteor.publish('FoodTrucksCollection', function publish() {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return FoodTrucksCollection.find({ owner: username });
+  }
+  return this.ready();
+});
+
+Meteor.publish('HoursCollection', function publish() {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return HoursCollection.find({ owner: username });
+  }
+  return this.ready();
+});
+
+Meteor.publish('LocationCollection', function publish() {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return LocationCollection.find({ owner: username });
+  }
+  return this.ready();
+});
+
+Meteor.publish('MenuItemCollection', function publish() {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return MenuItemCollection.find({ owner: username });
+  }
+  return this.ready();
+});
+
+Meteor.publish('RestaurantCollection', function publish() {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return RestaurantCollection.find({ owner: username });
+  }
+  return this.ready();
+});
+
+Meteor.publish('ReviewsCollection', function publish() {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return ReviewsCollection.find({ owner: username });
   }
   return this.ready();
 });
