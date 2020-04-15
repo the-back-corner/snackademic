396f3d0a5f4393637e8dbcf80e0c0b2b5c753894
Devoider <Drakon3001@gmail.com>
2020-04-14 21:41:42 -1000

    DNW Trying to display foodtruck name in map tab

--- app/imports/ui/components/RestaurantLocationItem.jsx
+++ app/imports/ui/components/RestaurantLocationItem.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-class StuffItem extends React.Component {
+class RestaurantLocationItem extends React.Component {
   render() {
     return (
         <Table.Row>
-          <Table.Cell>{this.props.stuff.name}</Table.Cell>
-          <Table.Cell>test</Table.Cell>
+          <Table.Cell>{this.props.foodTruck.name}</Table.Cell>
+          <Table.Cell> test
+          </Table.Cell>
         </Table.Row>
     );
   }
 }
 
 /** Require a document to be passed to this component. */
-StuffItem.propTypes = {
-  stuff: PropTypes.object.isRequired,
+RestaurantLocationItem.propTypes = {
+  foodTruck: PropTypes.object.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
-export default withRouter(StuffItem);
+export default withRouter(RestaurantLocationItem);
