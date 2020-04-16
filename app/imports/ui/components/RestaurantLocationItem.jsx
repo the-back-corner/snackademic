f06efe85e5b9917dfa12c2ae50b22342821948f3
Devoider <Drakon3001@gmail.com>
2020-04-16 11:17:14 -1000

    Now Displays Location and the name of the restaurant

--- app/imports/ui/components/RestaurantLocationItem.jsx
+++ app/imports/ui/components/RestaurantLocationItem.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import { moment } from 'meteor/momentjs:moment';
 import { withRouter } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class RestaurantLocationItem extends React.Component {
   render() {
+    const dayOfTheWeek = moment().format('dddd').toLowerCase();
+    // console.log(this.props.restaurantLocation);
+    // console.log(this.props.restaurantLocation[dayOfTheWeek]);
     return (
         <Table.Row>
           <Table.Cell>{this.props.restaurant.name}</Table.Cell>
-          <Table.Cell> test
+          <Table.Cell> {this.props.restaurantLocation[dayOfTheWeek]}
           </Table.Cell>
         </Table.Row>
     );
@@ -19,6 +23,7 @@ class RestaurantLocationItem extends React.Component {
 /** Require a document to be passed to this component. */
 RestaurantLocationItem.propTypes = {
   restaurant: PropTypes.object.isRequired,
+  restaurantLocation: PropTypes.object.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
