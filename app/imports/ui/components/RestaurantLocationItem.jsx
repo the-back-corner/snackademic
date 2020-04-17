7a66ca718fa5ad8db419df0c620ef8fe5802f57e
Devoider <Drakon3001@gmail.com>
2020-04-16 16:33:45 -1000

    Added hours to map page

--- app/imports/ui/components/RestaurantLocationItem.jsx
+++ app/imports/ui/components/RestaurantLocationItem.jsx
@@ -3,7 +3,6 @@ import { Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { moment } from 'meteor/momentjs:moment';
 import { withRouter } from 'react-router-dom';
-import { LocationCollection } from '../../api/location/LocationCollection';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class RestaurantLocationItem extends React.Component {
@@ -14,8 +13,8 @@ class RestaurantLocationItem extends React.Component {
     return (
         <Table.Row>
           <Table.Cell>{this.props.restaurant.name}</Table.Cell>
-          <Table.Cell> {this.props.restaurantLocation[dayOfTheWeek]}
-          </Table.Cell>
+          <Table.Cell> {this.props.restaurantLocation[dayOfTheWeek]}</Table.Cell>
+          <Table.Cell>{this.props.restaurantHours[dayOfTheWeek]}</Table.Cell>
         </Table.Row>
     );
   }
@@ -25,6 +24,7 @@ class RestaurantLocationItem extends React.Component {
 RestaurantLocationItem.propTypes = {
   restaurant: PropTypes.object.isRequired,
   restaurantLocation: PropTypes.object.isRequired,
+  restaurantHours: PropTypes.object.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -32,6 +32,7 @@ class App extends React.Component {
               <ProtectedRoute path="/map" component={Map}/>
               <ProtectedRoute path="/add" component={AddStuff}/>
               <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
+              <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
               <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
               <ProtectedRoute path="/signout" component={Signout}/>
               <Route component={NotFound}/>
