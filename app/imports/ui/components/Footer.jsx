3e2785de0e032872a8422721110d409fac37d971
Ramit Islam <53219044+ramytramit@users.noreply.github.com>
2020-04-29 18:54:37 -1000

    Bale menu added to DB, also alphabetized food truck names, fixed L&L spelling

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -10,7 +10,7 @@ class Footer extends React.Component {
           <div style={divStyle} className="ui center aligned container">
               Designed by <a href="https://github.com/the-back-corner">The Back Corner</a> <br />
               Team Members:  Clark Whitehead | David Harris | Jasmine Singleton | Ramit Islam<br />
-              University of Hawaii | Honolulu, HI 96822 <br />
+              University of Hawaii | Honolulu, Hawaii <br />
           </div>
           </div>
         </footer>
