46c01f1a033feac0343bc94f0faae1591a8d2508
Jasmine Singleton <jrsingleton@github.com>
2020-05-01 18:21:24 -1000

    added accordian to keep menu items in in food truck and restaurant
    also added icons and a group of buttons

--- app/imports/ui/pages/FoodTruck.jsx
+++ app/imports/ui/pages/FoodTruck.jsx
@@ -1,84 +1,101 @@
 import React from 'react';
-import { Card, Grid, Header, Image, Loader } from 'semantic-ui-react';
+import { Accordion, Button, Card, Grid, Header, Icon, Image, Loader } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { FoodTrucksCollection } from '../../api/foodTrucks/FoodTrucksCollection';
 import { MenuItemCollection } from '../../api/menu/MenuItemCollection';
 
-
 /** A simple static component to render some text for the landing page. */
 class FoodTruck extends React.Component {
+  state = { activeIndex: 0 };
 
-    /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+
+    this.setState({ activeIndex: newIndex });
+  };
+
+  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
 
   renderPage() {
-      console.log(this.props.doc2);
+    const { activeIndex } = this.state;
+    console.log(this.props.doc2);
     return (
         <div className="signupPage">
+          {/*First grid at top of page, holds food truck name and buttons*/}
           <Grid verticalAlign='middle' textAlign='center'>
             <Grid.Column>
               <Header className="cuisine" as='h1'>{this.props.doc.name}</Header>
+              <Button.Group>
+                <Button basic>
+                  <Button.Content visible as='h3'><Icon name='heart' color='blue'/>Add to Favorites</Button.Content>
+                  {/*  If user is logged in button will add the restaurant to their favorites on click
+                    if it is already in their favorites, button will save remove from favorites
+                     if user is not logged in button links to sign up page*/}
+                </Button>
+                <Button basic>
+                  <Button.Content as='h3'><Icon name='star' color='blue'/> Write A Review</Button.Content>
+                </Button>
+                <Button basic>
+                  <Button.Content as='h3'><Icon name='fork' color='blue'/> Share </Button.Content>
+                </Button>
+              </Button.Group>
             </Grid.Column>
           </Grid>
 
-
+          {/*Second grid holds food truck icon on the left, and description on the right*/}
           <Grid verticalAlign='middle' textAlign='center' container>
             <Grid.Column className="leftGrid" textAlign='left' width={8}>
-                  <Image src={this.props.doc.vendorIcon} size = "medium" verticalAlign='middle'/>
+              <Image src={this.props.doc.vendorIcon} size="medium" verticalAlign='middle'/>
             </Grid.Column>
+            <Grid.Column className="rightGrid" width={8}>
+              <Header className="cuisine" as='h1'>{this.props.doc.typeOfCuisine}</Header>
+              <Header className="secondHeader" as='h2'>{this.props.doc.Description}</Header>
+              {this.props.doc.takesMeals === 1 ?
+                  (<Header className="secondHeader" as='h3'>Takes Meal Points</Header>)
+                  : (<Header className="secondHeader" as='h3'>Does Not Take Meal Points</Header>)}
 
-                  {/*<Header className="firstHeader" as='h1'>Menu Items</Header>*/}
-                  {/*{ this.props.doc2.map((menuItem) => {*/}
-                  {/*    if (menuItem.restaurantName === this.props.doc.name){*/}
-                  {/*        return (*/}
-                  {/*            <Header key={menuItem._id} className="secondHeader" as='h3'>{menuItem.itemName} -*/}
-                  {/*                ${menuItem.price}</Header>*/}
-                  {/*         );*/}
-                  {/*    }*/}
-                  {/*    return (<Header key={menuItem._id}></Header>);*/}
-                  {/*  })*/}
-                  {/*}*/}
-              {/*</Grid.Column>*/}
-              <Grid.Column className="rightGrid" width={8}>
-                  <Header className="cuisine" as='h1'>{this.props.doc.typeOfCuisine}</Header>
-                  <Header className="secondHeader" as='h2'>{this.props.doc.Description}</Header>
-                  { this.props.doc.takesMeals === 1 ?
-                      (<Header className="secondHeader" as='h3'>Takes Meal Points</Header>)
-                      : (<Header className="secondHeader" as='h3'>Does Not Take Meal Points</Header>)}
+            </Grid.Column>
+          </Grid>
 
-              </Grid.Column>
-
-
-
-              <Grid.Column width={8}>
-              </Grid.Column>
-
-            </Grid>
+          {/* third grid holds Menu items*/}
           <Grid verticalAlign='middle' textAlign='center'>
             <Grid.Column>
-              <Header className="firstHeader" as='h1'>Menu Items</Header>
-              <Card.Group centered>
-                {this.props.doc2.map((menuItem) => {
-                  if (menuItem.restaurantName === this.props.doc.name) {
-                    return (
-                        <Card key={menuItem._id} className="secondHeader">
-                          <Card.Content>
-                            <Card.Header>
-                              {menuItem.itemName} -
-                              ${menuItem.price}
-                            </Card.Header>
-                          </Card.Content>
-                        </Card>
-                    );
-                  }
-                  return (<Header key={menuItem._id}></Header>);
-                })
-                }
-              </Card.Group>
+              <Accordion fluid styled>
+                <Accordion.Title
+                    active={activeIndex === 1}
+                    index={1}
+                    onClick={this.handleClick}
+                >
+                  <Header className="firstHeader"><Icon name='dropdown' />Menu Items</Header>
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 1}>
+                  <Card.Group centered>
+                    {this.props.doc2.map((menuItem) => {
+                      if (menuItem.restaurantName === this.props.doc.name) {
+                        return (
+                            <Card key={menuItem._id} className="secondHeader">
+                              <Card.Content>
+                                <Card.Header>
+                                  {menuItem.itemName} -
+                                  ${menuItem.price}
+                                </Card.Header>
+                              </Card.Content>
+                            </Card>
+                        );
+                      }
+                      return (<Header key={menuItem._id}></Header>);
+                    })
+                    }
+                  </Card.Group>
+                </Accordion.Content>
+              </Accordion>
             </Grid.Column>
           </Grid>
         </div>
@@ -90,22 +107,22 @@ class FoodTruck extends React.Component {
 
 /** Require the presence of a Stuff document in the props object. Uniforms adds 'model' to the props, which we use. */
 FoodTruck.propTypes = {
-    doc: PropTypes.object,
-    doc2: PropTypes.array,
-    ready: PropTypes.bool.isRequired,
+  doc: PropTypes.object,
+  doc2: PropTypes.array,
+  ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(({ match }) => {
-    // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
-    const documentId = match.params._id;
-    // Get access to Stuff documents.
-    const subscriptionTrucks = Meteor.subscribe('FoodTrucksCollection');
-    const subscriptionMenu = Meteor.subscribe('MenuItemCollection');
-    const subscriptionReviews = Meteor.subscribe('ReviewsCollection');
-    return {
-        doc: FoodTrucksCollection.findOne(documentId),
-        doc2: MenuItemCollection.find().fetch(),
-        ready: subscriptionTrucks.ready() && subscriptionMenu.ready() && subscriptionReviews.ready(),
-    };
+  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
+  const documentId = match.params._id;
+  // Get access to Stuff documents.
+  const subscriptionTrucks = Meteor.subscribe('FoodTrucksCollection');
+  const subscriptionMenu = Meteor.subscribe('MenuItemCollection');
+  const subscriptionReviews = Meteor.subscribe('ReviewsCollection');
+  return {
+    doc: FoodTrucksCollection.findOne(documentId),
+    doc2: MenuItemCollection.find().fetch(),
+    ready: subscriptionTrucks.ready() && subscriptionMenu.ready() && subscriptionReviews.ready(),
+  };
 })(FoodTruck);
--- app/imports/ui/pages/Restaurant.jsx
+++ app/imports/ui/pages/Restaurant.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Header, Image, Loader, Card, Container } from 'semantic-ui-react';
+import { Grid, Header, Image, Loader, Card, Button, Icon, Accordion } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
@@ -8,19 +8,42 @@ import { MenuItemCollection } from '../../api/menu/MenuItemCollection';
 
 /** A simple static component to render some text for the landing page. */
 class Restaurant extends React.Component {
+  state = { activeIndex: 0 };
 
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+
+    this.setState({ activeIndex: newIndex });
+  };
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
 
   renderPage() {
+    const { activeIndex } = this.state;
     console.log(this.props.doc2);
     return (
         <div className="signinPage">
           <Grid verticalAlign='middle' textAlign='center'>
             <Grid.Column>
               <Header className="cuisine" as='h1'>{this.props.doc.name}</Header>
+              <Button.Group>
+                <Button basic>
+                  <Button.Content visible as='h3'><Icon name='heart outline' color='blue'/>Add to Favorites</Button.Content>
+                  {/*  If user is logged in button will add the restaurant to their favorites on click
+                    if it is already in their favorites, button will save remove from favorites
+                     if user is not logged in button links to sign up page*/}
+                </Button>
+                <Button basic>
+                  <Button.Content as='h3'><Icon name='star outline' color='blue'/> Write A Review</Button.Content>
+                </Button>
+                <Button basic>
+                  <Button.Content as='h3'><Icon name='share square outline' color='blue'/> Share </Button.Content>
+                </Button>
+              </Button.Group>
             </Grid.Column>
           </Grid>
 
@@ -33,33 +56,44 @@ class Restaurant extends React.Component {
             <Grid.Column className="rightGrid" width={8}>
               <Header className="cuisine" as='h1'>{this.props.doc.typeOfCuisine}</Header>
               <Header className="secondHeader" as='h2'>{this.props.doc.Description}</Header>
-              {this.props.doc.takesMeals === 1 ? (<Header className="secondHeader" as='h3'>Takes Meal Points</Header>)
-                  : (<Header className="secondHeader" as='h3'>Does Not Take Meal Points</Header>)}
+              {this.props.doc.takesMeals === 1 ? (<Header className="secondHeader" as='h3'><Icon name='money bill alternate outline'/>Takes Meal Points</Header>)
+                  : (<Header className="secondHeader" as='h3'> <Icon name='money bill alternate outline'/> Does Not Take Meal Points</Header>)}
 
             </Grid.Column>
           </Grid>
 
           <Grid verticalAlign='middle' textAlign='center'>
             <Grid.Column>
-              <Header className="firstHeader" as='h1'>Menu Items</Header>
-              <Card.Group centered>
-              {this.props.doc2.map((menuItem) => {
-                if (menuItem.restaurantName === this.props.doc.name) {
-                  return (
-                      <Card key={menuItem._id} className="secondHeader">
-                        <Card.Content>
-                          <Card.Header>
-                            {menuItem.itemName} -
-                            ${menuItem.price}
-                          </Card.Header>
-                        </Card.Content>
-                      </Card>
-                  );
-                }
-                return (<Header key={menuItem._id}></Header>);
-              })
-              }
-              </Card.Group>
+
+              <Accordion fluid styled>
+                <Accordion.Title
+                    active={activeIndex === 1}
+                    index={1}
+                    onClick={this.handleClick}
+                >
+                  <Header className="firstHeader"><Icon name='dropdown' />Menu Items</Header>
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 1}>
+                  <Card.Group centered>
+                    {this.props.doc2.map((menuItem) => {
+                      if (menuItem.restaurantName === this.props.doc.name) {
+                        return (
+                            <Card key={menuItem._id} className="secondHeader">
+                              <Card.Content>
+                                <Card.Header>
+                                  {menuItem.itemName} -
+                                  ${menuItem.price}
+                                </Card.Header>
+                              </Card.Content>
+                            </Card>
+                        );
+                      }
+                      return (<Header key={menuItem._id}></Header>);
+                    })
+                    }
+                  </Card.Group>
+                </Accordion.Content>
+              </Accordion>
             </Grid.Column>
           </Grid>
         </div>
