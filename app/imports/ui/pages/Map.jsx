58e0c36f057ba6b9c636ecb114c14ee87859768d
Devoider <Drakon3001@gmail.com>
2020-05-08 02:08:23 -1000

    working static map and table

--- app/imports/ui/pages/Map.jsx
+++ app/imports/ui/pages/Map.jsx
@@ -3,11 +3,13 @@ import { Meteor } from 'meteor/meteor';
 import { Container, Table, Header, Loader, Image } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import { moment } from 'meteor/momentjs:moment';
 import { FoodTrucksCollection } from '../../api/foodTrucks/FoodTrucksCollection';
 import { RestaurantCollection } from '../../api/restaurant/RestaurantCollection';
 import { LocationCollection } from '../../api/location/LocationCollection';
 import { HoursCollection } from '../../api/hours/HoursCollection';
 import RestaurantLocationItem from '../components/RestaurantLocationItem';
+
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class Map extends React.Component {
@@ -19,12 +21,43 @@ class Map extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
-     const src = 'images/maps/snackademic-map-no-rotating-trucks.jpg';
+    const dayOfTheWeek = moment().format('dddd').toLowerCase();
+    let src = '';
+    // just the location
+    // the location and the rotation (for the pin)
+    switch (dayOfTheWeek) {
+      case 'monday':
+        src = 'images/maps/snackademic-map-mon.jpg';
+        break;
+      case 'tuesday':
+        src = 'images/maps/snackademic-map-tue.jpg';
+        break;
+      case 'wednesday':
+        src = 'images/maps/snackademic-map-wed.jpg';
+        break;
+      case 'thursday':
+        src = 'images/maps/snackademic-map-thu.jpg';
+        break;
+      case 'friday':
+        src = 'images/maps/snackademic-map-fri.jpg';
+        break;
+      default:
+        src = '';
+        break;
+    }
     return (
         <Container>
           <Header as="h2" textAlign="center">Map</Header>
-          <div>
-          <Image src={src} rounded size='massive'/>
+          <Image centered src={src} rounded size='massive'/>
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Restaurant</Table.HeaderCell>
+                <Table.HeaderCell>Location</Table.HeaderCell>
+                <Table.HeaderCell>Hours</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
               {this.props.foodTrucksCollection.map((foodTruck) => {
                 const restaurantLocation = LocationCollection.find({ restaurantName: foodTruck.name }).fetch()[0];
                 const restaurantHours = HoursCollection.find({ restaurantName: foodTruck.name }).fetch()[0];
@@ -51,7 +84,9 @@ class Map extends React.Component {
                     />);
                 })
               }
-          </div>
+
+            </Table.Body>
+          </Table>
         </Container>
     );
   }
