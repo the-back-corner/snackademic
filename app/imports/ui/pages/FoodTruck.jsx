fb531e67f7604b7bb0f7d5c4198089c4a0123ed2
Clark Whitehead <clark12341@gmail.com>
2020-05-09 01:57:09 -1000

    update

--- app/imports/ui/pages/FoodTruck.jsx
+++ app/imports/ui/pages/FoodTruck.jsx
@@ -34,6 +34,20 @@ class FoodTruck extends React.Component {
     });
   };
 
+  favoritesDelete = () => {
+    const currentUser = Meteor.user().username;
+    const currentName = (this.props.doc.name);
+    const favoritesArray = (this.props.docFavorites);
+    console.log("favorite clicked");
+    console.log(currentName);
+    console.log(currentUser);
+    console.log(favoritesArray);
+    FavoritesCollection.insert({
+      userName: currentUser,
+      restaurantName: currentName,
+    });
+  };
+
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
@@ -49,11 +63,15 @@ class FoodTruck extends React.Component {
               <Header className="cuisine" as='h1'>{this.props.doc.name}</Header>
               <Button.Group>
                 <Button inverted>
-                  <Button.Content as='h3' onClick={this.favoritesAdd}><Icon name='heart' color='blue'/>Add to Favorites</Button.Content>
-                  {/*<Button.Content as='h3' onClick={this.favoritesAdd}><Icon name='heart' color='blue'/>Add to Favorites</Button.Content>*/}
-                  {/*  If user is logged in button will add the restaurant to their favorites on click
-                    if it is already in their favorites, button will save remove from favorites
-                     if user is not logged in button links to sign up page */}
+                  {this.props.docFavorites.map((restaurantName) => {
+                    if (restaurantName.restaurantName === this.props.doc.name) {
+                      return (
+                          <Button.Content as='h3' onClick={this.favoritesDelete}><Icon name='heart' color='blue'/>Remove from Favorites</Button.Content>
+                      );
+                    }
+                    return (<Button.Content as='h3' onClick={this.favoritesAdd}><Icon name='heart' color='blue'/>Add to Favorites</Button.Content>);
+                  })
+                  }
                 </Button>
                 <Button inverted>
                   <Button.Content as='h3'><Icon name='star' color='blue'/> Write A Review</Button.Content>
